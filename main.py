77bb528b8afa66a5f877e1a6ea98cc6f400177c1
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-02-16 05:15:13 +0100

    fixed first task

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ request_queue = queue.Queue()
 
 # Функція для генерації заявок
 def generate_request():
-    request = "Request" + str(request_queue.qsuze() + 1)
+    request = "Request" + str(request_queue.qsize() + 1)
     request_queue.put(request)
     print(f"Заявка {request} додана до черги")
 
 # Функція для обробки заявок
-    def process_request():
-        if not request_queue.empty():
-            request = request_queue.get()
-            print(f"Заявка {request} оброблена")
-        else:
-            print("Черга порожня")
+def process_request():
+    if not request_queue.empty():
+        request = request_queue.get()
+        print(f"Заявка {request} оброблена")
+    else:
+        print("Черга порожня")
 
 # Головний цикл програми
-            while True:
-                generate_request()
-                process_request()
+while True:
+    generate_request()
+    process_request()
 
 # Завдання 2: Перевірка на паліндром
                 
-def is_palidrome(s):
+def is_palindrome(s):
     s = s.lower().replace(" ", "")
     char_queue = deque(s)
 
@@ -40,8 +40,8 @@ def is_palidrome(s):
 print("////////////////////////////////")
 input_string_false = "Hi! Jeg hedder Artem!"
 print(input_string_false)
-print(is_palidrome(input_string_false))
+print(is_palindrome(input_string_false))
 print("////////////////////////////////")
 input_string_true = "A man a plan a canal Panama"
 print(input_string_true)
-print(is_palidrome(input_string_true))
+print(is_palindrome(input_string_true))
